49ab645f3a0840890d9089acf3d887fa20c895db
cn-ljb <cn_ljb@sina.cn>
2021-03-14 14:14:40 +0800

    删除过滤

---
+++ home/src/main/java/com/ljb/android/component/home/common/Constant.kt
@@ -0,0 +1,20 @@
+package com.ljb.android.component.home.common
+
+
+
+/**
+ * 常量池
+ * Created by L on 2017/7/11.
+ */
+object Constant {
+
+
+    /**
+     *  SharedPreferences常量池
+     * */
+    object SPKey {
+        const val KEY_HOME_CACHE = "KEY_HOME_CACHE"
+    }
+
+
+}
---
+++ user/src/main/java/com/ljb/android/component/user/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ljb.android.component.user
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.ljb.android.component.user.view.act.UserLoginActivity
+import com.ljb.android.component.user.view.fragment.UserLeftDrawerFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btn_user).setOnClickListener { openDrawLeft() }
+        findViewById<View>(R.id.btn_login).setOnClickListener { goLogin() }
+    }
+
+    private fun goLogin() {
+        startActivity(Intent(this, UserLoginActivity::class.java))
+    }
+
+    private fun openDrawLeft() {
+        findViewById<View>(R.id.fl_draw_left).visibility = View.VISIBLE
+        val beginTransaction = supportFragmentManager.beginTransaction()
+        beginTransaction.replace(R.id.fl_draw_left, UserLeftDrawerFragment())
+        beginTransaction.commit()
+    }
+
+}
